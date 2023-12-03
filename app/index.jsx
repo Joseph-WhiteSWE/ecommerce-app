b75c5dd8d100bf05ed71036677c94333ce7f9170
joseph <white.joseph.jr@gmail.com>
2023-12-02 19:53:53 -0600

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
